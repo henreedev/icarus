feaf31f21aefa37a243a1d8c766d043ac2921698
Earnest <hearnest@ad.brown.edu>
2024-06-21 18:26:21 -0400

    flap and glide changes

--- scenes/characters/icarus.gd
+++ scenes/characters/icarus.gd
@@ -28,6 +28,7 @@ var speed := 0.0
 var doing_flap = false
 var charging_flap = false
 var flap_on_cooldown = false
+var applied_flap_impulse = false
 var flap_type : Flap = Flap.NONE
 var flap_charge_time := 0.0
 
@@ -39,6 +40,7 @@ var hoz_speed = MIN_HOZ_SPEED
 # Onready variables
 @onready var main : Main = get_tree().get_first_node_in_group("main")
 @onready var flap_timer : Timer = $FlapTimer
+@onready var asprite : AnimatedSprite2D = $AS
 
 #endregion
 
@@ -48,7 +50,6 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	_process_state(delta)
-	print("Info: ", flap_type, flap_on_cooldown, doing_flap)
 
 func _physics_process(delta: float) -> void:
 	speed = linear_velocity.length()
@@ -61,6 +62,7 @@ func _process_state(delta : float) -> void:
 		State.FLAPPING:
 			if doing_flap and flap_type == Flap.NORMAL:
 				if Input.is_action_just_pressed("flap"):
+					applied_flap_impulse = false
 					_flap()
 			if not doing_flap:
 				if Input.is_action_just_pressed("flap"):
@@ -70,13 +72,15 @@ func _process_state(delta : float) -> void:
 			if charging_flap:
 				flap_charge_time += delta
 		State.GLIDING:
-			pass
+			if Input.is_action_just_pressed("flap"):
+				_tumble()
 		State.FALLING:
 			pass
 
 func _physics_process_state(delta) -> void:
 	match state:
 		State.FLAPPING:
+			_rotate_on_input(delta)
 			if doing_flap:
 				match flap_type:
 					Flap.WEAK:
@@ -88,58 +92,160 @@ func _physics_process_state(delta) -> void:
 					Flap.NONE:
 						print("ERROR Flap.NONE encountered")
 		State.GLIDING:
-			pass
+			_rotate_on_input(delta)
+			var rotation_vector = Vector2.from_angle(rotation)
+			linear_velocity = linear_velocity.length() * rotation_vector
+			_pick_animation()
 		State.FALLING:
-			pass
+			pass # TODO add height check for losing the game
+
 
 func _begin_flapping():
-	pass
+	state = State.FLAPPING
+	_pick_animation()
 
 func _begin_gliding():
-	pass
+	state = State.GLIDING
+	_pick_animation()
+	_reset_flap_vars()
+	linear_damp = 0.0
 
 func _begin_falling():
-	pass
+	state = State.FALLING
+	_pick_animation()
+	_reset_flap_vars()
 #endregion
 
 #region: Helper functions
 func _flap_timer(duration : float):
 	flap_timer.start(duration)
 	await flap_timer.timeout
+
+func _reset_flap_vars():
+	applied_flap_impulse = false
+	doing_flap = false
+	flap_on_cooldown = false
+	flap_type = Flap.NONE
+	linear_damp = 1.0
+
+
+func apply_upward_force(magnitude):
+	apply_central_force(Vector2(0, -magnitude).rotated(rotation))
+
+func apply_upward_impulse(magnitude):
+	apply_central_impulse(Vector2(0, -magnitude).rotated(rotation))
+
+
+func _pick_animation():
+	match state:
+		State.FLAPPING:
+			if doing_flap:
+				match flap_type:
+					Flap.WEAK:
+						asprite.animation = "flap_weak"
+					Flap.NORMAL:
+						asprite.animation = "flap_normal"
+					Flap.STRONG:
+						asprite.animation = "flap_strong"
+			else:
+				asprite.animation = "float"
+		State.GLIDING:
+			if rotation_degrees > 10.0: asprite.animation = "glide_down"
+			elif rotation_degrees < -10.0: asprite.animation = "glide_up"
+			elif asprite.animation != "glide_down" and asprite.animation != "glide_up":
+				asprite.animation = "glide_up"
+		State.FALLING:
+			asprite.animation = "fall"
+	asprite.play()
+
 #endregion
 
 #region: Action functions
+func _rotate_on_input(delta : float) -> void:
+	match state:
+		State.FLAPPING:
+			const ROTATION_SPEED = 30.0
+			const ROTATION_LIMIT = 15.0
+			var rotation_adj = 0.0
+			if Input.is_action_pressed("lean_left"):
+				rotation_adj -= ROTATION_SPEED
+			if Input.is_action_pressed("lean_right"):
+				rotation_adj += ROTATION_SPEED
+			apply_torque(rotation_adj)
+			rotation_degrees = clampf(rotation_degrees, -ROTATION_LIMIT, ROTATION_LIMIT)
+		State.GLIDING:
+			const ROTATION_SPEED = 45.0
+			const ROTATION_LIMIT = 45.0
+			var rotation_adj = 0.0
+			if Input.is_action_pressed("lean_left"):
+				rotation_adj -= ROTATION_SPEED
+			if Input.is_action_pressed("lean_right"):
+				rotation_adj += ROTATION_SPEED
+			apply_torque(rotation_adj)
+			rotation_degrees = clampf(rotation_degrees, -ROTATION_LIMIT, ROTATION_LIMIT)
+
+func _tumble() -> void:
+	asprite.animation = "tumble"
+	asprite.play()
 
 func _flap() -> void:
-	# TODO start flap animation
 	flap_type = _pick_flap_type()
 	flap_on_cooldown = true
 	doing_flap = true
 	match flap_type:
 		Flap.WEAK:
-			apply_central_impulse(Vector2(0, -100))
+			pass
 		Flap.NORMAL:
-			apply_central_impulse(Vector2(0, -200))
 			pass
 		Flap.STRONG:
-			apply_central_impulse(Vector2(0, -300))
 			pass
-	await get_tree().create_timer(0.25).timeout
-	if not charging_flap:
-		doing_flap = false
-	await get_tree().create_timer(0.25).timeout
-	if not charging_flap:
-		flap_on_cooldown = false
+	_pick_animation()
 
 func _do_weak_flap(delta : float):
-	doing_flap = false
+	const FORCE_STR = 100.0
+	const IMPULSE_STR = 100.0
+	match asprite.frame:
+		0: 
+			linear_damp = 10.0
+			apply_upward_force(-FORCE_STR)
+		1:
+			if not applied_flap_impulse:
+				applied_flap_impulse = true
+				apply_upward_impulse(IMPULSE_STR) 
+		2: 
+			linear_damp = 1.0
+			apply_upward_force(FORCE_STR)
 
 func _do_normal_flap(delta : float):
-	doing_flap = false
+	const FORCE_STR = 300.0
+	const IMPULSE_STR = 300.0
+	match asprite.frame:
+		0: linear_damp = 10.0
+		1:
+			if not applied_flap_impulse:
+				applied_flap_impulse = true
+				apply_upward_impulse(IMPULSE_STR) 
+		2, 3, 4: 
+			linear_damp = 1.0
+			apply_upward_force(FORCE_STR)
+		5: linear_damp = 10.0
 
 func _do_strong_flap(delta : float):
-	doing_flap = false
-
+	const IMPULSE_STR = 200.0
+	const FORCE_STR = 350.0
+	if asprite.animation == "tumble":
+		#match asprite.frame:
+		linear_damp = 0.0
+	else:
+		match asprite.frame:
+			0,1: linear_damp = 10.0
+			2: 
+				linear_damp = 1.0
+				if not applied_flap_impulse:
+					applied_flap_impulse = true
+					apply_upward_impulse(IMPULSE_STR)
+				apply_upward_force(FORCE_STR)
+			4: linear_damp = 1.0
 
 func _pick_flap_type() -> Flap:
 	if flap_on_cooldown:
@@ -150,9 +256,8 @@ func _pick_flap_type() -> Flap:
 		return Flap.NORMAL
 
 func _begin_flap() -> void:
-	# TODO start charge animation
 	charging_flap = true
-	flap_charge_time = 0
+	flap_charge_time = 0.0
 	flap_type = Flap.NONE
 
 func _release_flap() -> void:
@@ -161,3 +266,19 @@ func _release_flap() -> void:
 		
 		_flap()
 #endregion
+
+#region: Signal connection functions
+
+func _on_as_animation_finished() -> void:
+	match asprite.animation:
+		"flap_weak", "flap_normal":
+			_reset_flap_vars()
+			_pick_animation()
+		"flap_strong":
+			_tumble()
+		"tumble":
+			if state == State.FLAPPING:
+				_begin_gliding()
+			else:
+				_begin_flapping()
+
